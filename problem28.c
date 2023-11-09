415449ea84040a3e8114baf55cd7f9482ede6a6a
Heroyoush <114863287+heroyoush@users.noreply.github.com>
2023-11-09 08:28:44 +0800

    Add files via upload

---
+++ problem28.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void myName();
+
+int main()
+{
+    myName();
+
+    return 0;
+}
+
+    void myName()
+{
+    printf("*****************************************\n");
+    printf("\0\n");
+    printf("*\t\t\t\t\t*\n");
+    printf("*\tClint Justine A. Nepomuceno\t*\n");
+    printf("\0\n");
+    printf("*\t\t\t\t\t*\n");
+    printf("*****************************************\n");
+    return;
+}
