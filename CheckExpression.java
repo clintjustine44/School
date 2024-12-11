ee184443cd2a4430513a678febe516a287ca2556
clintjustine44 <114863287+clintjustine44@users.noreply.github.com>
2024-12-12 05:08:18 +0800

    Add CheckExpression.java

--- CheckExpression.java
+++ CheckExpression.java
@@ -1,39 +1,43 @@
 import java.io.*;
 import java.util.*;
 
-public class CheckExpression {
+public class SemiFinal{
     public static void main(String[] args) {
         String fileName = "expressions.txt";
         List<String> expressions = readExpressions(fileName);
-        for (String expression : expressions) {
-            if(isWellDefined(expression))
-                System.out.println("Well Defined");
-            else
-                System.out.println("Not Well Defined");
+
+        if(expressions == null){
+            System.out.println("There's an error reading the file.");
+        }else{
+            for (String exp : expressions) {
+                if(isWellDefined(exp))
+                    System.out.println("Well Defined");
+                else
+                    System.out.println("Not Well Defined");
+            }
         }
     }
 
-    public static List<String> readExpressions(String fileName) {
+    public static List<String> readExpressions(String fileName){
         List<String> expressions = new ArrayList<>();
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
             String line;
-            while ((line = reader.readLine()) != null) {
+            while((line = reader.readLine()) != null){
                 expressions.add(line);
             }
         } catch (IOException e) {
             return null;
         }
-
         return expressions;
     }
 
     public static boolean isWellDefined(String expression){
         Stack<Character> stack = new Stack<>();
         for (char ch : expression.toCharArray()) {
-            if(ch == '{' || ch == '(' || ch == '[')
+            if(ch == '(' || ch == '[' || ch == '{')
                 stack.push(ch);
-            else if(ch == '}' || ch == ')' || ch == ']'){
+            else if(ch == ')' || ch == ']' || ch == '}'){
                 if(stack.isEmpty())
                     return false;
                 char lastOpened = stack.pop();
@@ -44,9 +48,7 @@ public class CheckExpression {
         return stack.isEmpty();
     }
 
-    public static boolean matches(char open, char close) {
-        return (open == '(' && close == ')' ||
-                open == '[' && close == ']' ||
-                open == '{' && close == '}');
+    public static boolean matches(char open, char close){
+        return(open == '(' && close == ')' || open == '[' && close == ']' || open == '{' && close == '}');
     }
 }
