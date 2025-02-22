0209d6d13f8d7e71e2191d2bb9323051c6aaf9ff
clintjustine44 <114863287+clintjustine44@users.noreply.github.com>
2025-02-22 10:01:21 +0800

    Update ProblemSet1.cs

--- ProblemSet1.cs
+++ ProblemSet1.cs
@@ -1,104 +1,58 @@
 using System;
 
-public class Point{
-    public double X { get; set; }
-    public double Y { get; set; }
-
-    public Point(double x, double y){
-        X = x;
-        Y = y;
-    }
-}
-
 class Program{
     public static void Main(string[] args){
-        Point[] points = {
-            new Point(2, 3),
-            new Point(12, 30),
-            new Point(40, 50),
-            new Point(5, 1),
-            new Point(12, 10),
-            new Point(3, 4)
-        };
-
-        double closestDistance = ClosestPair(points);
-        Console.WriteLine($"Closest Distance: {closestDistance:F2}");
+        int[] arr = { 8, 4, 2, 1 };
+        int inversionCount = CountInversion(arr);
+        Console.WriteLine("Number of inversions: " + inversionCount);
     }
 
-    static double ClosestPair(Point[] points){
-        // Sort points by x-coordinate
-        Array.Sort(points, (p1, p2) => p1.X.CompareTo(p2.X));
-        return ClosestPairUtil(points, 0, points.Length - 1);
+    static int CountInversion(int[] arr){
+        int[] temp = new int[arr.Length];
+        return MergeSortAndCount(arr, temp, 0, arr.Length - 1);
     }
 
-    static double ClosestPairUtil(Point[] points, int left, int right){
-        // Base case: if there are 3 or fewer points, use brute force
-        if (right - left <= 3){
-            return BruteForce(points, left, right);
-        }
+    static int MergeAndCount(int[] arr, int[] temp, int left, int mid, int right){
+        int i = left, j = mid + 1, k = left;
+        int invCount = 0;
 
-        // Find the mid point
-        int mid = (left + right) / 2;
-        double midX = points[mid].X;
-
-        // Find the smallest distance in both halves
-        double d1 = ClosestPairUtil(points, left, mid);
-        double d2 = ClosestPairUtil(points, mid + 1, right);
-        double d = Math.Min(d1, d2);
-
-        // Create an array to hold points within d of the midline
-        Point[] strip = new Point[right - left + 1];
-        int stripCount = 0;
-
-        for (int i = left; i <= right; i++){
-            if (Math.Abs(points[i].X - midX) < d){
-                strip[stripCount++] = points[i];
+        while (i <= mid && j <= right){
+            if (arr[i] <= arr[j]){
+                temp[k++] = arr[i++];
+            }
+            else{
+                temp[k++] = arr[j++];
+                invCount += (mid - i + 1); // Count inversions
             }
         }
 
-        // Sort the strip according to y-coordinate using a simple bubble sort
-        for (int i = 0; i < stripCount - 1; i++){
-            for (int j = 0; j < stripCount - i - 1; j++){
-                if (strip[j].Y > strip[j + 1].Y){
-                    Point temp = strip[j];
-                    strip[j] = strip[j + 1];
-                    strip[j + 1] = temp;
-                }
-            }
+        while (i <= mid){
+            temp[k++] = arr[i++];
         }
 
-        // Find the closest points in the strip
-        return Math.Min(d, StripClosest(strip, stripCount, d));
-    }
-
-    static double BruteForce(Point[] points, int left, int right){
-        double minDist = double.MaxValue;
-        for (int i = left; i <= right; i++){
-            for (int j = i + 1; j <= right; j++){
-                double dist = Distance(points[i], points[j]);
-                if (dist < minDist){
-                    minDist = dist;
-                }
-            }
+        while (j <= right){
+            temp[k++] = arr[j++];
         }
-        return minDist;
-    }
-
-    static double StripClosest(Point[] strip, int size, double d){
-        double minDist = d;
-
-        for (int i = 0; i < size; i++){
-            for (int j = i + 1; j < size && (strip[j].Y - strip[i].Y) < minDist; j++){
-                double dist = Distance(strip[i], strip[j]);
-                if (dist < minDist){
-                    minDist = dist;
-                }
-            }
+        
+        for (i = left; i <= right; i++){
+            arr[i] = temp[i];
         }
-        return minDist;
+
+        return invCount;
     }
 
-    static double Distance(Point p1, Point p2){
-        return Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+    static int MergeSortAndCount(int[] arr, int[] temp, int left, int right){
+        int mid, invCount = 0;
+
+        if (left < right){
+            mid = left + (right - left) / 2;
+
+            // Divide step
+            invCount += MergeSortAndCount(arr, temp, left, mid);
+            invCount += MergeSortAndCount(arr, temp, mid + 1, right);
+            invCount += MergeAndCount(arr, temp, left, mid, right);
+        }
+
+        return invCount;
     }
 }
